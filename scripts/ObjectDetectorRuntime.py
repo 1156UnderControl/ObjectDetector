4816d018d8c5c7d5eda65bbbb9dfe1127b5c1a06
HenriqueSchmitz <schhenrique@gmail.com>
2021-01-29 22:06:08 -0300

    Changed default IP and organized Runtime python file

--- scripts/ObjectDetectorRuntime.py
+++ scripts/ObjectDetectorRuntime.py
@@ -5,7 +5,8 @@ from XML import XML
 import socket
 import time
 
-        
+########## FUNÇÕES DA CAMERA ##########
+
 def startCamera(detectionXMLName):
     camera = PiCamera()
     cameraConfig = XML(detectionXMLName)
@@ -21,28 +22,39 @@ def takePicture():
     camera.capture(rawCapture, format="bgr")
     return rawCapture.array
 
+
+########## LEITURA DE CONFIGURAÇÕES ##########
+
 runtimeConfiguration = XML("/home/pi/Desktop/scripts/runtime.xml")
 detectionXMLName = "/home/pi/Desktop/object-detection/" + runtimeConfiguration.getText("configurationXML")
-print("Detection configuration file: " + detectionXMLName)
-camera, rawCapture = startCamera(detectionXMLName)
-objectDetector = ObjectDetector(detectionXMLName)
 ip = runtimeConfiguration.getText("ip")
 port = runtimeConfiguration.getValue("port")
-print("UDP Target: " + ip + ":" + port)
+
+
+########## INICIALIZAÇÕES ##########
+
+camera, rawCapture = startCamera(detectionXMLName)
+objectDetector = ObjectDetector(detectionXMLName)
 UDPSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-
 isDetected = False
 counter = 0
 
+
+########## PRINT DE STATUS ##########
+
+print("Detection configuration file: " + detectionXMLName)
+print("UDP Target: " + ip + ":" + str(port))
+
+
+########## LOOP DE EXECUÇÃO ##########
+
 while True:
-    if not isDisabled:
-        image = takePicture()
-        isDetected = objectDetector.evaluate(image)
-        byte_message = bytes("isDetected:" + str(isDetected), "utf-8")
-        UDPSocket.sendto(byte_message, (ip, port))
+    image = takePicture()
+    isDetected = objectDetector.evaluate(image)
+    byte_message = bytes("isDetected:" + str(isDetected), "utf-8")
+    UDPSocket.sendto(byte_message, (ip, port))
     time.sleep(0.1)
     counter += 1
     if counter >= 10:
         counter = 0
-        print("[STATUS] isDisabled: " + isDisabled + "\t\tisDetected: " + isDetected)
+        print("[STATUS] isDetected: " + str(isDetected))
